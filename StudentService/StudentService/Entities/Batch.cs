7c6073de5ede8c849ea71137bce9b6b4a4ba3f2b
Kyogre <96482135+kiet15092003@users.noreply.github.com>
2025-05-20 20:24:37 +0700

    remove student service

--- StudentService/StudentService/Entities/Batch.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentService.Entities
-{
-    public class Batch : BaseEntity
-    {
-        public string Title { get; set; }
-        public int StartYear { get; set; }
-        public List<Student> Students { get; set; }
-    }
-}
