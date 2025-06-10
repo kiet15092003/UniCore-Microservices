9e780da80273f9e673f6757192319744b29b2f50
truongthq <96380633+QuangTruong0603@users.noreply.github.com>
2025-06-11 05:58:53 +0700

    fix material upload

--- UserService/UserService/Business/Dtos/Lecturer/UpdateLecturerDto.cs
+++ UserService/UserService/Business/Dtos/Lecturer/UpdateLecturerDto.cs
@@ -5,35 +5,26 @@ namespace UserService.Business.Dtos.Lecturer
 {
     public class UpdateLecturerDto
     {
-        [Required]
         public string FirstName { get; set; }
         
-        [Required]
         public string LastName { get; set; }
         
-        [Required]
         public string PersonId { get; set; }
         
-        [Required]
         [Phone]
         [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number must be exactly 10 digits")]
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must contain exactly 10 digits")]
         public string PhoneNumber { get; set; }
         
-        [Required]
         public string Dob { get; set; }
         
-        [Required]
         public string Degree { get; set; }
         
-        [Required]
-        public decimal Salary { get; set; }
+        public decimal? Salary { get; set; }
         
-        [Required]
-        public Guid DepartmentId { get; set; }
+        public Guid? DepartmentId { get; set; }
         
-        [Required]
-        public int WorkingStatus { get; set; }
+        public int? WorkingStatus { get; set; }
         
         public string MainMajor { get; set; }
         
